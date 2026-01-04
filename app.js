afbe22e2a2c44fd581fd65c59e466ac57227f051
OrlandoC017 <orlando.castillo7123@gmail.com>
2026-01-04 18:56:39 -0500

    working on search function

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=2d9420f2&s={search word}
+const moviesListEl = document.querySelector(".movie-list");
 
-
-async function main() {
-    const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=2d9420f2&s=CARS")
+async function onSearchChange(event) {
+    const searchTerm = document.querySelector('.input').value;
+    const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=2d9420f2&s=${searchTerm}`)
     const moviesData = await movies.json();
-    const moviesListEl = document.querySelector(".movie-list");
+    const movie = event.target.value
+    console.log(event.target.value)
 
-    moviesListEl.innerHTML = moviesData
-
-    
-
-    moviesData.map(
-        (movie) => `<div class="movie__profile">
+    moviesListEl.innerHTML = moviesData.Search.map((movie) =>
+         `<div class="movie__profile">
             <figure class="movie__poster--wrapper">
                 <img src="${movie.Poster}" alt="" class="movie__poster--img">
             </figure>
@@ -25,5 +23,3 @@ async function main() {
     ).join('')
     
 }
-
-main();
