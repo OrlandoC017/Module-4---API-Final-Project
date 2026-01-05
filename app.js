0a45a44acee949f5d4d33933ca8b0dbdcc46f704
OrlandoC017 <orlando.castillo7123@gmail.com>
2026-01-05 10:51:57 -0500

    working on filter

--- app.js
+++ app.js
@@ -5,25 +5,46 @@ async function onSearchChange(event) {
     const searchTerm = document.querySelector('.input').value;
     const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=2d9420f2&s=${searchTerm}`)
     const moviesData = await movies.json();
-    const movie = event.target.value
-    console.log(searchTerm)
-
-    if (movie.Poster === "N/A") {
-        movie.Poster = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+    
+    if (event.target.value === "NEWEST_FIRST") {
+        moviesData.Search.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+    } else if (event.target.value === "OLDEST_FIRST") {
+        moviesData.Search.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+    } else if (event.target.value === "A_TO_Z") {
+        moviesData.Search.sort((a, b) => a.Title.localeCompare(b.Title));
+    } else if (event.target.value === "Z_TO_A") {
+        moviesData.Search.sort((a, b) => b.Title.localeCompare(a.Title));
     }
 
-    moviesListEl.innerHTML = moviesData.Search.map((movie) =>
-         `<div class="movie__profile">
+    if (moviesData.Search) {
+        moviesListEl.innerHTML = moviesData.Search.map((movie) => {
+            const posterUrl = movie.Poster === "N/A"
+            ? "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+            : movie.Poster;
+
+            const typeCapitalized = movie.Type.charAt(0).toUpperCase() + movie.Type.slice(1); 
+
+            return `<div class="movie__profile">
             <figure class="movie__poster--wrapper">
-                <img src="${movie.Poster}" alt="" class="movie__poster--img">
+                <img src="${posterUrl}" alt="" onerror="this.onerror = null; this.src = '${posterUrl}'" class="movie__poster--img">
             </figure>
             <h2 class="movie__title">${movie.Title}</h2>
             <h3 class="movie__year">Theatrical Release: ${movie.Year}</h3>
-            <h3 class="movie__type">${movie.Type}</h3>
+            <h3 class="movie__type">Media: ${typeCapitalized}</h3>
             <form action="https://www.imdb.com/title/${movie.imdbID}/" target="_blank">
                 <input class ="movie__imdb click"  type="submit" value="IMDB" />
             </form>
         </div>`
-    ).join('')
-    
+    }).join('')
+    } else {
+        moviesListEl.innerHTML = `<img src="https://cdn.dribbble.com/userupload/2905341/file/original-e40e0ef8b2d32a9aaf5fcf21679966b4.png?resize=1024x768&vertical=center" alt="No Results Found" class="no__results--img"/>`
+    }
+
+       
+}
+
+
+function filterMedia(event) {
+    onSearchChange(event.target.value)
+
 }
